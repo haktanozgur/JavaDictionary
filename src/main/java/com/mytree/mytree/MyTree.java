bebd8ecde83a81a07724596b692625c9f284ef49
haktanozgur <haktanozur10@gmail.com>
2022-03-11 13:37:44 +0300

    Son Değişiklik

--- src/main/java/com/mytree/mytree/MyTree.java
+++ src/main/java/com/mytree/mytree/MyTree.java
@@ -6,16 +6,19 @@ import java.io.*;
 public class MyTree {
 
     public static void main(String[] args) {
-    
-    try {
       System.out.println("Sözlük yükleniyor ....");
-      File myObj = new File("sozluk.txt");      
-      Scanner myReader = new Scanner(myObj); 
+      File myObj = new File("sozluk.txt"); 
       System.out.println("Sözlük yüklendi.");
+      
+ 
+ for(;;){   
+    try {     
+      Scanner myReader = new Scanner(myObj); 
       String kelime2 = readInput();
       System.out.println(kelime2);
     while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
+
         
     if(data.toLowerCase().startsWith(kelime2.toLowerCase())){
         System.out.println(data);
@@ -23,18 +26,20 @@ public class MyTree {
             }
     
         }
+ 
     
     catch (FileNotFoundException e) {
       System.out.println("Bir Hata Oluştu !");
       e.printStackTrace();
         } 
 
-        
+ } 
     }
     public static String readInput() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Bir Kelime Yazıp Enter Tuşuna Basınız ");
         String kelime = null;
+        
         
     try {
         kelime = reader.readLine();
